2903e9fc092cedc25e2317fdf884636830a40746
HenrikSwahn <h_swahn@hotmail.com>
2015-05-18 10:43:23 +0200

    Done some more testing

--- src/main/Frame.java
+++ src/main/Frame.java
@@ -1,5 +1,7 @@
 package main;
 
+import org.w3c.dom.ranges.RangeException;
+
 /**
  * Created by Nilj on 18/05/15.
  */
@@ -7,16 +9,18 @@ public class Frame {
 
     private int[] pins;
 
-    public Frame() {
-
-        this.pins = new int[2];
-        this.pins[0] = -1;
-        this.pins[1] = -1;
-
-    }
-
     public Frame(int val1, int val2) {
 
+
+        if(val1 < 0 || val2 < 0) {
+            throw new RangeException((short)1,"Invalid range");
+        }
+
+        int sum = val1 + val2;
+
+        if(sum > 10) {
+            throw new RangeException((short)1,"Invalid range");
+        }
         this.pins = new int[2];
         this.pins[0] = val1;
         this.pins[1] = val2;
@@ -25,5 +29,9 @@ public class Frame {
 
     public int getArr(int pos) {
         return this.pins[pos];
+    }
+
+    public int getScore() {
+        return this.pins[0] + this.pins[1];
     }
 }
--- src/test/FrameTest.java
+++ src/test/FrameTest.java
@@ -1,34 +1,81 @@
 package test;
 
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import static org.junit.Assert.*;
 import main.Frame;
+import org.w3c.dom.ranges.RangeException;
 
 public class FrameTest {
 
     private Frame f;
+    private int val1 = 5;
+    private int val2 = 1;
 
     @Before
     public void setup() {
-        f = new Frame();
-    }
-
-    @Test
-    public void test_default_constructor() {
-
-        assertEquals(f.getArr(0), -1);
-        assertEquals(f.getArr(1), -1);
-
+        f = new Frame(val1,val2);
     }
 
     @Test
     public void test_constructor() {
 
-        Frame f2 = new Frame(5,1);
+        assertEquals(val1, f.getArr(0));
+        assertEquals(val2, f.getArr(1));
 
-        assertEquals(f2.getArr(0),5);
-        assertEquals(f2.getArr(1),1);
+    }
+
+    //Test for FrameScore
+    @Test
+    public void constructor_GivenInvalidPositiveRange_ExpectedException() {
+
+        Throwable e = null;
+        Frame f;
+        try {
+            f = new Frame(50,50);
+        }catch(Throwable ex) {
+            e = ex;
+        }
+
+        assertTrue(e instanceof RangeException);
+    }
+
+    //Test for FrameScore
+    @Test
+    public void constructor_GivenInvalidNegativeRange_ExpectedException() {
+
+        Throwable e = null;
+        Frame f;
+        try {
+            f = new Frame(-50,-25);
+        }catch(Throwable ex) {
+            e = ex;
+        }
+
+        assertTrue(e instanceof RangeException);
+    }
+
+    //Test for FrameScore
+    @Test
+    public void constructor_GivenBadValuesValidSum_ExpectedException() {
+
+        Throwable e = null;
+        Frame f;
+        try {
+            f = new Frame(-50,50);
+        }catch(Throwable ex) {
+            e = ex;
+        }
+
+        assertTrue(e instanceof RangeException);
+    }
+
+    @Test
+    public void getScore_GivenSumOfVal1AndVal2_ExpectedSum() {
+
+        int result = f.getScore();
+        assertEquals(6, result);
 
     }
 }
